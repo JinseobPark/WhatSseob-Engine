4f2e9a92bd4b1ec37f31a925badd9c4933600e76
JPark <0814owl@gmail.com>
2021-07-09 21:31:34 +0900

    texture + batch
    batcher in texture.
    so I can load image batchly manymany

--- Whatsseob-core/main.cpp
+++ Whatsseob-core/main.cpp
@@ -22,7 +22,7 @@
 #include <time.h>
 
 #define BATCH_RENDERER 1
-#define TEST_50K_SPRITES 0
+#define TEST_50K_SPRITES 1
 
 #if 1
 int main(void)
@@ -37,48 +37,25 @@ int main(void)
 
 	mat4 ortho = mat4::orthographic(16.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 	Shader* s1 =  new Shader("src/shaders/basic.vert", "src/shaders/basic.frag");
-	Shader* s2 = new Shader("src/shaders/basic.vert", "src/shaders/basic.frag");
 	Shader& shader = *s1;
-	Shader& shader2 = *s2;
 	shader.enable();
-	shader2.enable();
 	shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
-	shader2.setUniform2f("light_pos", vec2(4.0f, 1.5f));
+
 
 	TileLayer layer(&shader);
-#if TEST_50K_SPRITES
-	for (float y = -9.0f; y < 9.0f; y += 0.1)
+
+	for (float y = -9.0f; y < 9.0f; y ++)
 	{
-		for (float x = -16.0f; x < 16.0f; x += 0.1)
+		for (float x = -16.0f; x < 16.0f; x ++)
 		{
-			layer.add(new Sprite(x, y, 0.09f, 0.09f, maths::vec4(rand() % 1000 / 1000.0f, rand() % 1000 / 1000.0f, 1.0f, 1.0f)));
+			layer.add(new Sprite(x, y, 0.9f, 0.9f, maths::vec4(rand() % 1000 / 1000.0f, rand() % 1000 / 1000.0f, 1.0f, 1.0f)));
 		}
 	}
-#else
-	mat4 transform = mat4::rotation(45.0f, vec3(0, 0, 1)) * mat4::translation(vec3(-15.0f, 5.0f, 0.0f));
-	Group* group = new Group(transform);
-	group->add(new Sprite(0, 0, 6, 3, maths::vec4(1, 1, 1, 1)));
-	group->add(new Sprite(0.5f, 0.5f, 5.0f, 2.0f, maths::vec4(1, 0, 1, 1)));
 
-	Group* button = new Group(mat4::translation(vec3(0.5f, 0.5f, 0.0f)));
-	button->add(new Sprite(0, 0, 6, 3, maths::vec4(1, 1, 1, 1)));
-	button->add(new Sprite(0.5f, 0.5f, 5.0f, 2.0f, maths::vec4(1, 0, 1, 1)));
-	//group->add(button);
-
-	layer.add(group);
-	layer.add(button);
-
-#endif
-
-	TileLayer layer2(&shader2);
-	layer2.add(new Sprite(-2, -2, 4, 4, maths::vec4(1, 1, 1, 1)));
 	glActiveTexture(GL_TEXTURE0);
 
-	Texture texture("test.png");
+	Texture texture("test2.png");
 	texture.bind();
-
-	//Simple2DRenderer simple;
-	//Sprite* testTexture = new Sprite(0, 0, 4, 4, maths::vec4(1, 1, 1, 1));
 
 	shader.enable();
 	shader.setUniform1i("tex", 0);
@@ -90,29 +67,13 @@ int main(void)
 	{
 		window.clear();
 
-		/*
 		double x, y;
 		window.getMousePosition(x, y);
 		shader.setUniform2f("light_pos", vec2((float)(x * 32.0f / 960.f - 16.0f), (float)(9.0f - y * 18.0f / 540.f)));
-		shader2.enable();
-		//shader2.setUniform2f("light_pos", vec2((float)(x * 32.0f / 960.f - 13.0f), (float)(9.0f - y * 18.0f / 540.f)));
 
 		layer.render();
-		//layer2.render();
-		*/
-		glBegin(GL_QUADS);
-		glTexCoord2f(0, 0);
-		glVertex2f(0, 0);
-		glTexCoord2f(0, 1);
-		glVertex2f(0, 4);
-		glTexCoord2f(1, 1);
-		glVertex2f(4, 4);
-		glTexCoord2f(1, 0);
-		glVertex2f(4, 0);
-		glEnd();
 
-		//simple.submit(testTexture);
-		//simple.flush();
+
 		window.update();
 		frames++;
 		if (time.elapsed() - timer > 1.0f)
